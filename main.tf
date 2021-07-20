6e7e3aa0dbe33dd37627494888e60100738da295
viv-garot <vivien@hashicorp.com>
2021-07-20 13:48:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"    
+    organization = "vivienTFtraining"    
     workspaces {
       name = "learn-terraform-pipelines-k8s"
     }
